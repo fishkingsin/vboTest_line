24c21cb4235a781698d8d661f84cd4775ed3a6e4
James Kong <fishkingsin@gmail.com>
2013-02-27 11:14:35 +0800

    init commit

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ int main( ){
     ofAppGlutWindow window;
 	ofSetupOpenGL(&window, 1024,768, OF_WINDOW);			// <-------- setup the GL context
 #else
-	ofSetupOpenGL(1024,768, OF_WINDOW);
+	ofSetupOpenGL(1920,1080, OF_WINDOW);
 #endif
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
